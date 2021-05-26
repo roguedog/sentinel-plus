593aa842598ea713818c4ddfde8cbe9ef3fcf1f2
kaizi2009 <kaizi_2009@126.com>
2021-05-26 20:00:23 +0800

    init

--- src/main/java/org/github/rd/sentinel/App.java
+++ src/main/java/org/github/rd/sentinel/App.java
@@ -2,11 +2,10 @@ package org.github.rd.sentinel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.config.EnableWebFlux;
-import reactor.netty.http.server.HttpServer;
+import org.springframework.context.annotation.Import;
 
-@EnableWebFlux
 @SpringBootApplication
+@Import(Config.class)
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
--- src/main/java/org/github/rd/sentinel/Config.java
+++ src/main/java/org/github/rd/sentinel/Config.java
@@ -1,33 +1,82 @@
 package org.github.rd.sentinel;
 
-import com.sun.jdi.connect.Connector;
+import io.netty.channel.ChannelOption;
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.reactive.server.ReactiveWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.http.client.reactive.ReactorResourceFactory;
+import org.springframework.http.server.reactive.HttpHandler;
+import reactor.netty.http.server.HttpServer;
+import reactor.netty.resources.LoopResources;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 
+
+//https://cloud.tencent.com/developer/ask/180811
+//https://blog.csdn.net/weixin_34383618/article/details/89590931
+//netty自定义工作线程 https://blog.csdn.net/weixin_43364172/article/details/89210706
+//webflux官方文档 https://docs.spring.io/spring-framework/docs/current/reference/html/web-reactive.html#webflux
+//reactor netty官方文档 https://projectreactor.io/docs/netty/release/reference/index.html 配置方法https://projectreactor.io/docs/netty/release/reference/index.html#_tcp_level_configuration
+//netty参数配置详解https://blog.csdn.net/qq_33797928/article/details/107205995?utm_medium=distribute.pc_relevant.none-task-blog-baidujs_title-0&spm=1001.2101.3001.4242 https://www.jianshu.com/p/975b30171352 https://blog.csdn.net/WillPan1234/article/details/58598220
+//netty资源隔离配置https://blog.csdn.net/weixin_43364172/article/details/88720097
+//NettyWebServerFactoryCustomizer
+
+//设置线程的代码块 org.springframework.boot.autoconfigure.web.embedded.TomcatWebServerFactoryCustomizer.customizeMaxThreads
+//容器初始化的入口 EmbeddedWebServerFactoryCustomizerAutoConfiguration
 @Configuration
 public class Config {
+
+    /*@Autowired
+    HttpHandler httpHandler;
+
+    WebServer http1;
+
+    @PostConstruct
+    public void start() {
+
+        NettyReactiveWebServerFactory nettyFactory = new NettyReactiveWebServerFactory(8000);
+        nettyFactory.setResourceFactory(null);
+
+        nettyFactory.addServerCustomizers(server ->
+                server.runOn(LoopResources.create("server", 1, 8, true))
+        );
+        ReactorResourceFactory reactorResourceFactory = new ReactorResourceFactory();
+        reactorResourceFactory.setLoopResources(LoopResources.create("server", 1, 8, true));
+
+        nettyFactory.setResourceFactory(reactorResourceFactory);
+        this.http1 = nettyFactory.getWebServer(this.httpHandler);
+        this.http1.start();
+
+    }
+
+    @PreDestroy
+    public void stop() {
+        this.http1.stop();
+    }*/
+
     @Bean
-    public RouterFunction<ServerResponse> initRouterFunction(){
-        return RouterFunctions.route()
-                .GET("/hello/{name}",serverRequest -> {
-                    String name=serverRequest.pathVariable("name");
-
-                    return ServerResponse.ok().bodyValue(name);
-                })
-                .POST("/hello2",serverRequest -> {
-                    String name=serverRequest.exchange().getRequest().getQueryParams().getFirst("name");
-
-                    assert name != null;
-                    return ServerResponse.ok().bodyValue(name);
-                })
-                .build();
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(1111);
+        int[] ports = {8000, 8100};
+        List<Connector> connectors = new ArrayList<>();
+        for (int port:ports){
+            Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+            connector.setPort(port);
+            connector.setScheme("http");
+            connectors.add(connector);
+        }
+        tomcat.addAdditionalTomcatConnectors(connectors.toArray(connectors.toArray(new Connector[] {})));
+        tomcat.setPort(9999);
+        return tomcat;
     }
 
 }
