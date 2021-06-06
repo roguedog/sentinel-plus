90bc58fa8250236aafcacbd18b192e5a7d94d4f8
kaizi2009 <kaizi_2009@126.com>
2021-06-06 16:09:48 +0800

    webflux配置

--- rd-sentinel-dashboard-rule-datasource/src/main/java/com/github/rd/sentinel/dashboard/datasource/App.java
+++ rd-sentinel-dashboard-rule-datasource/src/main/java/com/github/rd/sentinel/dashboard/datasource/App.java
@@ -1,17 +1,30 @@
 package com.github.rd.sentinel.dashboard.datasource;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import reactor.core.publisher.Mono;
-import reactor.netty.DisposableServer;
-import reactor.netty.http.server.HttpServer;
-//netty使用文档 https://projectreactor.io/docs/netty/release/reference/index.html#_starting_and_stopping_2
-//
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.embedded.netty.NettyReactiveWebServerFactory;
+
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+//        runJettyServer();
+    }
+
+    static void runJettyServer() {
+        new NettyReactiveWebServerFactory();
+        JettyServletWebServerFactory jettyServletWebServerFactory = new JettyServletWebServerFactory(8000);
+        jettyServletWebServerFactory.getWebServer().start();
+        JettyServletWebServerFactory jettyServletWebServerFactory1 = new JettyServletWebServerFactory(8100);
+        jettyServletWebServerFactory1.getWebServer().start();
+    }
+
+    /*static void runNettyServer() {
+    //netty使用文档 https://projectreactor.io/docs/netty/release/reference/index.html#_starting_and_stopping_2
         //reactor-netty-server的reactor.netty.http.server.HttpServerBind是单例模式，只能创建一个Http服务
-        DisposableServer server = HttpServer.create()
+        DisposableServer server = new MyNettyHttpServer()
 //                .option(ChannelOption) //自定义配置TCP
 //                .channelGroup() //广播消息
                 .accessLog(true)
@@ -23,6 +36,5 @@ public class App {
                 }))
                 .bindNow();
         server.onDispose().block();
-
-    }
+    }*/
 }
---
+++ rd-sentinel-dashboard-rule-datasource/src/main/java/com/github/rd/sentinel/dashboard/datasource/TestApi.java
@@ -0,0 +1,15 @@
+package com.github.rd.sentinel.dashboard.datasource;
+
+/*import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestApi {
+
+    @GetMapping("test")
+    public Object test() {
+        return "hello";
+    }
+}*/
